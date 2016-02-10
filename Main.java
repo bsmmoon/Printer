054b7a5bfbbbfe762f4abfc50e89cdd6eca09f3c
SEOKMIN MOON <SEOKMIN MOON>
2016-02-10 20:59:30 +0800

    Reading algo

--- Main.java
+++ Main.java
@@ -6,10 +6,23 @@ class Printer {
     private int pageNumber = 0;
 
     public Printer() {
-        
+
+    }
+
+    private void printParagraph(String paragraph) {
+
     }
 
     public void print(StringBuffer input) {
-        
+        String paragraph;
+        int from = 0;
+        int to = input.length();
+        while (input.indexOf("\n", from) != -1) {
+            to = input.indexOf("\n", from) + 1; // include linebreaker
+            paragraph = input.substring(from, to);
+            printParagraph(paragraph);
+            from = to;
+        }
+        printParagraph(from, input.length());
     }
 }
