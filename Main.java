2fbe0453e81824d17a4ee5f453e6e2aeaf4e6159
SEOKMIN MOON <SEOKMIN MOON>
2016-02-10 20:50:07 +0800

    skeleton

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.lang.StringBuffer;
+
+class Printer {
+    private int WIDTH;
+    private int HEIGHT;
+    private int pageNumber = 0;
+
+    public Printer() {
+        
+    }
+
+    public void print(StringBuffer input) {
+        
+    }
+}
